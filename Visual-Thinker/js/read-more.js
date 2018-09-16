250c41c61410bbf669ef18ab3a03eca82c6df943
Breno-Silva1 <bbreno100@live.com>
2018-09-16 13:39:01 -0300

    Modificando página do projeto

---
+++ Visual-Thinker/js/read-more.js
@@ -0,0 +1,21 @@
+$('.wrapper').removeClass('maxheight');
+
+$('button').click(function() {
+  $(this).animate({
+    top: '-10px'
+  }, 150, 'easeInOutCubic');
+  $(this).animate({
+    top: '10px'
+  }, 150, 'easeInOutCubic', function() {
+    $(this).toggleClass('readless');
+    $(this).parent().children('.container').children('.wrapper').children('.background').toggle();
+    $(this).parent().children('.container').children('.wrapper').toggleClass('maxheight');
+
+    if ($(this).hasClass('readless')) {
+      $(this).text("Ver Menos");
+    } else {
+      $(this).text("Ver Mais");
+    };
+  });
+
+});
