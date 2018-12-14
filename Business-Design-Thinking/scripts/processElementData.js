8c16fc255e7beb3fe12639505df08d05a8304e89
20171014040018 <breno.oliveira@academico.ifrn.edu.br>
2018-12-14 07:13:06 -0300

    :)

---
+++ Business-Design-Thinking/scripts/processElementData.js
@@ -0,0 +1,112 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQActivity"]="./../../Business Design Thinking/deliveryprocesses/resources/Business Design Thinking_EDBD48BD_edbd48bd_Activity.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_GL1Dwf9VEeiaSoXD8gf5XQ,_2kXxQf9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Documento de Visão Geral_B6E02A1_3da13400_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_JziKQf9VEeiaSoXD8gf5XQ,_BE4iof9WEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Prototipação Interativa E Em Alta Fidelidade_584B45A3_4f434482_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_GL1Dwf9VEeiaSoXD8gf5XQ,_7pssUf9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Reuniões com o Cliente_670CAEFD_993fe05c_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_JziKQf9VEeiaSoXD8gf5XQ,_CQINwf9WEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Desenvolvimento Das Interfaces Do Sistema_E9D9D9B6_e0d1d895_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_HYqDQf9VEeiaSoXD8gf5XQ,_9hDPgf9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Desenvolvimento Da Solução_3C90A58D_a479ba3e_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_M6lawf9VEeiaSoXD8gf5XQ,_DuLgIf9WEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Plano De Teste_B03487DD_d64a28a2_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_FSeZ4f9VEeiaSoXD8gf5XQ,_0AoCIf9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Documentação de Resultados_3FF3AD5_134834e1_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_M6lawf9VEeiaSoXD8gf5XQ,_Ep6vof9WEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Entrega Ao Cliente_4A7855B6_708df67b_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_JziKQf9VEeiaSoXD8gf5XQ,__t4Tcf9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Prototipação Em Baixa E Média Fidelidade_75DA73CB_6cd272aa_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_GL1Dwf9VEeiaSoXD8gf5XQ,_3WKakf9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Ambiente De Desenvolvimento_4F4ADB28_817e0c87_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_FSeZ4f9VEeiaSoXD8gf5XQ,_yaYdEf9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Briefing com Cliente_7C06C787_8b4fc193_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1bQpkf9UEeiaSoXD8gf5XQ,_FSeZ4f9VEeiaSoXD8gf5XQ,_1Pea4f9VEeiaSoXD8gf5XQActivityDetail"]="./../../Business Design Thinking/deliveryprocesses/resources/Elaborar Canvas do Projeto_3528E060_4471da6c_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["__t4Tcf9VEeiaSoXD8gf5XQ"] = ["Prototipação Em Baixa E Média Fidelidade ","Business Design Thinking/deliveryprocesses/Prototipação Em Baixa E Média Fidelidade_75DA73CB.html"];
+contentPage.processPage.elementUrls["_GEASkP9XEeiaSoXD8gf5XQ"] = ["Avaliar Usabilidade do Protótipo Interativo Junto ao Cliente","Business Design Thinking/tasks/Avaliar Usabilidade do Protótipo Interativo Junto ao Cliente_5BD0B9EC.html"];
+contentPage.processPage.elementUrls["_m8dLxf9WEeiaSoXD8gf5XQ"] = ["Gerente de Projetos","Business Design Thinking/roles/Gerente de Projetos_D2F7ED1D.html"];
+contentPage.processPage.elementUrls["_gNpxcf9WEeiaSoXD8gf5XQ"] = ["Gerente de Projetos","Business Design Thinking/roles/Gerente de Projetos_D2F7ED1D.html"];
+contentPage.processPage.elementUrls["_EUsbpv9YEeiaSoXD8gf5XQ"] = ["Cronograma de Reuniões","Business Design Thinking/workproducts/Cronograma de Reuniões_F02822AF.html"];
+contentPage.processPage.elementUrls["_jwAd4P9WEeiaSoXD8gf5XQ"] = ["Formalizar Briefing do Cliente","Business Design Thinking/tasks/Formalizar Briefing do Cliente_5558ED9A.html"];
+contentPage.processPage.elementUrls["_GL1Dwf9VEeiaSoXD8gf5XQ"] = ["Definição","Business Design Thinking/deliveryprocesses/Definição_AB44A3FA.html"];
+contentPage.processPage.elementUrls["_DuLgIf9WEeiaSoXD8gf5XQ"] = ["Plano De Teste","Business Design Thinking/deliveryprocesses/Plano De Teste_B03487DD.html"];
+contentPage.processPage.elementUrls["_jwAd5f9WEeiaSoXD8gf5XQ"] = ["Anotações do Projeto","Business Design Thinking/workproducts/Anotações do Projeto_6A2D8988.html"];
+contentPage.processPage.elementUrls["_2l3JkP9WEeiaSoXD8gf5XQ"] = ["Detalhar Requisitos","Business Design Thinking/tasks/Detalhar Requisitos_1ADC10CE.html"];
+contentPage.processPage.elementUrls["_LCIZcP9XEeiaSoXD8gf5XQ"] = ["Desenvolvedores e Testadores","Business Design Thinking/roles/Desenvolvedores e Testadores_D231DF4C.html"];
+contentPage.processPage.elementUrls["_7yDhoP9WEeiaSoXD8gf5XQ"] = ["Definir Arquitetura de Desenvolvimento","Business Design Thinking/tasks/Definir Arquitetura de Desenvolvimento_E975AC7A.html"];
+contentPage.processPage.elementUrls["_A7FQ8P9XEeiaSoXD8gf5XQ"] = ["Desenvolver Protótipo Das Interfaces Principais Em Média Fidelidade","Business Design Thinking/tasks/Desenvolver Protótipo Das Interfaces Principais Em Média Fidelid_7658D533.html"];
+contentPage.processPage.elementUrls["_M7lOYf9XEeiaSoXD8gf5XQ"] = ["Desenvolvedores e Testadores","Business Design Thinking/roles/Desenvolvedores e Testadores_D231DF4C.html"];
+contentPage.processPage.elementUrls["_GEASkf9XEeiaSoXD8gf5XQ"] = ["Stakeholders","Business Design Thinking/roles/Stakeholders_2355C6E0.html"];
+contentPage.processPage.elementUrls["__nb3wP9WEeiaSoXD8gf5XQ"] = ["Esboçar Protótipo Das Interfaces Em Baixa Fidelidade","Business Design Thinking/tasks/Esboçar Protótipo Das Interfaces Em Baixa Fidelidade_461DE620.html"];
+contentPage.processPage.elementUrls["_BdPi1P9XEeiaSoXD8gf5XQ"] = ["Desenvolvedores e Testadores","Business Design Thinking/roles/Desenvolvedores e Testadores_D231DF4C.html"];
+contentPage.processPage.elementUrls["_2kXxQf9VEeiaSoXD8gf5XQ"] = ["Documento de Visão Geral","Business Design Thinking/deliveryprocesses/Documento de Visão Geral_B6E02A1.html"];
+contentPage.processPage.elementUrls["_LCHyYP9XEeiaSoXD8gf5XQ"] = ["Definir Fluxos a Serem Seguidos","Business Design Thinking/tasks/Definir Fluxos a Serem Seguidos_FDE3BD35.html"];
+contentPage.processPage.elementUrls["_FSeZ4f9VEeiaSoXD8gf5XQ"] = ["Empatia","Business Design Thinking/deliveryprocesses/Empatia_C44363A7.html"];
+contentPage.processPage.elementUrls["_BE4iof9WEeiaSoXD8gf5XQ"] = ["Prototipação Interativa E Em Alta Fidelidade","Business Design Thinking/deliveryprocesses/Prototipação Interativa E Em Alta Fidelidade_584B45A3.html"];
+contentPage.processPage.elementUrls["_CQINwf9WEeiaSoXD8gf5XQ"] = ["Desenvolvimento Das Interfaces Do Sistema","Business Design Thinking/deliveryprocesses/Desenvolvimento Das Interfaces Do Sistema_E9D9D9B6.html"];
+contentPage.processPage.elementUrls["_88FEw_9WEeiaSoXD8gf5XQ"] = ["Stakeholders","Business Design Thinking/roles/Stakeholders_2355C6E0.html"];
+contentPage.processPage.elementUrls["_jwAd4f9WEeiaSoXD8gf5XQ"] = ["Gerente de Projetos","Business Design Thinking/roles/Gerente de Projetos_D2F7ED1D.html"];
+contentPage.processPage.elementUrls["_EUsbpP9YEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_FHbVkf9XEeiaSoXD8gf5XQ"] = ["Gerente de Projetos","Business Design Thinking/roles/Gerente de Projetos_D2F7ED1D.html"];
+contentPage.processPage.elementUrls["_hQqtoP9WEeiaSoXD8gf5XQ"] = ["Esboçar Desejos","Business Design Thinking/tasks/Esboçar Desejos_89E9F8A9.html"];
+contentPage.processPage.elementUrls["_EUsboP9YEeiaSoXD8gf5XQ"] = ["Gerente de Projetos","Business Design Thinking/roles/Gerente de Projetos_D2F7ED1D.html"];
+contentPage.processPage.elementUrls["_88FExf9WEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_FgWL4P9XEeiaSoXD8gf5XQ"] = ["Desenvolver Protótipo das Interfaces de Forma Interativa","Business Design Thinking/tasks/Desenvolver Protótipo das Interfaces de Forma Interativa_1F926C86.html"];
+contentPage.processPage.elementUrls["_g7a2YP9WEeiaSoXD8gf5XQ"] = ["Esboçar Solução","Business Design Thinking/tasks/Esboçar Solução_2D5D3482.html"];
+contentPage.processPage.elementUrls["_m8dLw_9WEeiaSoXD8gf5XQ"] = ["Stakeholders","Business Design Thinking/roles/Stakeholders_2355C6E0.html"];
+contentPage.processPage.elementUrls["_88FEwP9WEeiaSoXD8gf5XQ"] = ["Definir Cronograma De Reuniões","Business Design Thinking/tasks/Definir Cronograma De Reuniões_6E6CE8DD.html"];
+contentPage.processPage.elementUrls["_BdPi0v9XEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_FHbVk_9XEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_BdPi0P9XEeiaSoXD8gf5XQ"] = ["Gerente de Projetos","Business Design Thinking/roles/Gerente de Projetos_D2F7ED1D.html"];
+contentPage.processPage.elementUrls["_LUqT4P9XEeiaSoXD8gf5XQ"] = ["Indentificar e Concertar Possíveis Falhas","Business Design Thinking/tasks/Indentificar e Concertar Possíveis Falhas_E083B749.html"];
+contentPage.processPage.elementUrls["_3fp4UP9WEeiaSoXD8gf5XQ"] = ["Classificar Caso de Uso com Cliente","Business Design Thinking/tasks/Classificar Caso de Uso com Cliente_7EC82096.html"];
+contentPage.processPage.elementUrls["_HlukAP9XEeiaSoXD8gf5XQ"] = ["Criar Todas Interfaces Do Sistema","Business Design Thinking/tasks/Criar Todas Interfaces Do Sistema_B820C741.html"];
+contentPage.processPage.elementUrls["_FHbVkP9XEeiaSoXD8gf5XQ"] = ["Avaliar Protótipo Junto a Equipe de Desenvolvimento (Alta Fidelidade)","Business Design Thinking/tasks/Avaliar Protótipo Junto a Equipe de Desenvolvimento Alta Fidelid_2E1DF7D5.html"];
+contentPage.processPage.elementUrls["_hQqtof9WEeiaSoXD8gf5XQ"] = ["Anotações do Projeto","Business Design Thinking/workproducts/Anotações do Projeto_6A2D8988.html"];
+contentPage.processPage.elementUrls["_EUoKMP9YEeiaSoXD8gf5XQ"] = ["Definir Cronograma De Reuniões","Business Design Thinking/tasks/Definir Cronograma De Reuniões_6E6CE8DD.html"];
+contentPage.processPage.elementUrls["_3fp4Uf9WEeiaSoXD8gf5XQ"] = ["Stakeholders","Business Design Thinking/roles/Stakeholders_2355C6E0.html"];
+contentPage.processPage.elementUrls["_6miwwP9WEeiaSoXD8gf5XQ"] = ["Definir Caso de Uso Básico do Sistema","Business Design Thinking/tasks/Definir Caso de Uso Básico do Sistema_E39EF3FC.html"];
+contentPage.processPage.elementUrls["__nce0P9WEeiaSoXD8gf5XQ"] = ["Designer","Business Design Thinking/roles/Designer_285CCBA0.html"];
+contentPage.processPage.elementUrls["_6miwwf9WEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_1Pea4f9VEeiaSoXD8gf5XQ"] = ["Elaborar Canvas do Projeto","Business Design Thinking/deliveryprocesses/Elaborar Canvas do Projeto_3528E060.html"];
+contentPage.processPage.elementUrls["_Oak78P9XEeiaSoXD8gf5XQ"] = ["Avaliar Desenvoltura do Cliente ao Utilizar o Software","Business Design Thinking/tasks/Avaliar Desenvoltura do Cliente ao Utilizar o Software_DA3E91F8.html"];
+contentPage.processPage.elementUrls["_BdO7wP9XEeiaSoXD8gf5XQ"] = ["Avaliar Protótipo Junto a Equipe de Desenvolvimento (Média Fidelidade)","Business Design Thinking/tasks/Avaliar Protótipo Junto a Equipe de Desenvolvimento Média Fideli_ECF30F22.html"];
+contentPage.processPage.elementUrls["_jwAd4_9WEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_M7lOYP9XEeiaSoXD8gf5XQ"] = ["Produzir Manual do Usuário","Business Design Thinking/tasks/Produzir Manual do Usuário_D5D5629.html"];
+contentPage.processPage.elementUrls["_1bQpkf9UEeiaSoXD8gf5XQ"] = ["Business Design Thinking","Business Design Thinking/deliveryprocesses/Business Design Thinking_EDBD48BD.html"];
+contentPage.processPage.elementUrls["_19OcQf9WEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_ESIsUf9XEeiaSoXD8gf5XQ"] = ["Designer","Business Design Thinking/roles/Designer_285CCBA0.html"];
+contentPage.processPage.elementUrls["_5nEQwf9WEeiaSoXD8gf5XQ"] = ["Desenvolvedores e Testadores","Business Design Thinking/roles/Desenvolvedores e Testadores_D231DF4C.html"];
+contentPage.processPage.elementUrls["_m8dLwP9WEeiaSoXD8gf5XQ"] = ["Produzir Canvas Junto com o Cliente","Business Design Thinking/tasks/Produzir Canvas Junto com o Cliente_3244172B.html"];
+contentPage.processPage.elementUrls["_5nEQwP9WEeiaSoXD8gf5XQ"] = ["Avaliar Com Equipe Tecnologias a Serem Usadas","Business Design Thinking/tasks/Avaliar Com Equipe Tecnologias a Serem Usadas_F05C5885.html"];
+contentPage.processPage.elementUrls["_m8dLwf9WEeiaSoXD8gf5XQ"] = ["Analista","Business Design Thinking/roles/Analista_94B028C6.html"];
+contentPage.processPage.elementUrls["_EUsbov9YEeiaSoXD8gf5XQ"] = ["Stakeholders","Business Design Thinking/roles/Stakeholders_2355C6E0.html"];
+contentPage.processPage.elementUrls["_gNpxc_9WEeiaSoXD8gf5XQ"] = ["Stakeholders","Business Design Thinking/roles/Stakeholders_2355C6E0.html"];
+contentPage.processPage.elementUrls["_7yDhof9WEeiaSoXD8gf5XQ"] = ["Documento de Ambiente","Business Design Thinking/workproducts/Documento de Ambiente_C3CE611C.html"];
+contentPage.processPage.elementUrls["_M6lawf9VEeiaSoXD8gf5XQ"] = ["Teste","Business Design Thinking/deliveryprocesses/Teste_6BDB6560.html"];
+contentPage.processPage.elementUrls["_PDkOkP9XEeiaSoXD8gf5XQ"] = ["Registrar Possíveis Falhas Para Concertar","Business Design Thinking/tasks/Registrar Possíveis Falhas Para Concertar_E87C496D.html"];
+contentPage.processPage.elementUrls["_7pssUf9VEeiaSoXD8gf5XQ"] = ["Reuniões com o Cliente","Business Design Thinking/deliveryprocesses/Reuniões com o Cliente_670CAEFD.html"];
+contentPage.processPage.elementUrls["_gNpxcP9WEeiaSoXD8gf5XQ"] = ["Esboçar Problema","Business Design Thinking/tasks/Esboçar Problema_D110C003.html"];
+contentPage.processPage.elementUrls["_88FEwf9WEeiaSoXD8gf5XQ"] = ["Gerente de Projetos","Business Design Thinking/roles/Gerente de Projetos_D2F7ED1D.html"];
+contentPage.processPage.elementUrls["_yaYdEf9VEeiaSoXD8gf5XQ"] = ["Briefing com Cliente","Business Design Thinking/deliveryprocesses/Briefing com Cliente_7C06C787.html"];
+contentPage.processPage.elementUrls["_7gHdEP9WEeiaSoXD8gf5XQ"] = ["Avaliar Viabilidade da Arquitetura","Business Design Thinking/tasks/Avaliar Viabilidade da Arquitetura_F2E7D8A5.html"];
+contentPage.processPage.elementUrls["_jwAd5_9WEeiaSoXD8gf5XQ"] = ["Documento de Resultados do Briefing","Business Design Thinking/workproducts/Documento de Resultados do Briefing_53CA4DC3.html"];
+contentPage.processPage.elementUrls["_19OcQP9WEeiaSoXD8gf5XQ"] = ["Definir Escopo","Business Design Thinking/tasks/Definir Escopo_6154474C.html"];
+contentPage.processPage.elementUrls["_HYqDQf9VEeiaSoXD8gf5XQ"] = ["Idealização","Business Design Thinking/deliveryprocesses/Idealização_905CDD4C.html"];
+contentPage.processPage.elementUrls["_88FEx_9WEeiaSoXD8gf5XQ"] = ["Cronograma de Reuniões","Business Design Thinking/workproducts/Cronograma de Reuniões_F02822AF.html"];
+contentPage.processPage.elementUrls["_3WKakf9VEeiaSoXD8gf5XQ"] = ["Ambiente De Desenvolvimento","Business Design Thinking/deliveryprocesses/Ambiente De Desenvolvimento_4F4ADB28.html"];
+contentPage.processPage.elementUrls["_H9MBwP9XEeiaSoXD8gf5XQ"] = ["Acoplar Lógica Do Sistema As Interfaces","Business Design Thinking/tasks/Acoplar Lógica Do Sistema As Interfaces_25983F14.html"];
+contentPage.processPage.elementUrls["_HlvLEP9XEeiaSoXD8gf5XQ"] = ["Designer","Business Design Thinking/roles/Designer_285CCBA0.html"];
+contentPage.processPage.elementUrls["_PDkOkf9XEeiaSoXD8gf5XQ"] = ["Designer","Business Design Thinking/roles/Designer_285CCBA0.html"];
+contentPage.processPage.elementUrls["_7HI8YP9WEeiaSoXD8gf5XQ"] = ["Validar Arquitetura com CDU Básico","Business Design Thinking/tasks/Validar Arquitetura com CDU Básico_24352F1D.html"];
+contentPage.processPage.elementUrls["_9hDPgf9VEeiaSoXD8gf5XQ"] = ["Desenvolvimento Da Solução","Business Design Thinking/deliveryprocesses/Desenvolvimento Da Solução_3C90A58D.html"];
+contentPage.processPage.elementUrls["_Ep6vof9WEeiaSoXD8gf5XQ"] = ["Entrega Ao Cliente","Business Design Thinking/deliveryprocesses/Entrega Ao Cliente_4A7855B6.html"];
+contentPage.processPage.elementUrls["_FHbVlf9XEeiaSoXD8gf5XQ"] = ["Desenvolvedores e Testadores","Business Design Thinking/roles/Desenvolvedores e Testadores_D231DF4C.html"];
+contentPage.processPage.elementUrls["_3PofUP9WEeiaSoXD8gf5XQ"] = ["Agrupar Requisitos em Casos de Usos","Business Design Thinking/tasks/Agrupar Requisitos em Casos de Usos_C72979FD.html"];
+contentPage.processPage.elementUrls["_JziKQf9VEeiaSoXD8gf5XQ"] = ["Prototipagem","Business Design Thinking/deliveryprocesses/Prototipagem_9FB9F17A.html"];
+contentPage.processPage.elementUrls["_H9MBwf9XEeiaSoXD8gf5XQ"] = ["Desenvolvedores e Testadores","Business Design Thinking/roles/Desenvolvedores e Testadores_D231DF4C.html"];
+contentPage.processPage.elementUrls["_0AoCIf9VEeiaSoXD8gf5XQ"] = ["Documentação de Resultados","Business Design Thinking/deliveryprocesses/Documentação de Resultados_3FF3AD5.html"];
+contentPage.processPage.elementUrls["_ESIsUP9XEeiaSoXD8gf5XQ"] = ["Desenvolver Protótipo Das Interfaces Em Alta Fidelidade","Business Design Thinking/tasks/Desenvolver Protótipo Das Interfaces Em Alta Fidelidade_3BD1E6B5.html"];
+contentPage.processPage.elementUrls["_B1PzIP9XEeiaSoXD8gf5XQ"] = ["Ajustar Mudanças No Protótipo","Business Design Thinking/tasks/Ajustar Mudanças No Protótipo_2DFDA438.html"];
